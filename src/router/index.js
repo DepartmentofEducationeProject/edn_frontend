f49c06e0d52a0eb17cb4d5265bc14fa8f7889c9e
FuJinDeFengJing <1153421754@qq.com>
2021-03-31 00:29:24 +0800

    (1)搭建了前端VUE框架 (2)完成了简易的登陆界面和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '../views/Login.vue'
 import Home from '../views/Home.vue'
+import Test_1 from "../views/Test_1";
+import Test_2 from "../views/Test_2";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    children: [
+      {
+        path: '/test1',
+        name: 'Test1',
+        component: Test_1,
+      },
+      {
+        path: '/test2',
+        name: 'Test2',
+        component: Test_2,
+      }
+    ]
+  },
+
 ]
 
 const router = new VueRouter({
